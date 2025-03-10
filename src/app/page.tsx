157445992a0f3476d8cb15edb3211552e0963937
solemannoushad <solemannoushad31@gmail.com>
2025-03-10 17:47:22 +0500

    support section component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import SpecialitiesSection from "@/components/SpecialitiesSection";
+import SupportSection from "@/components/SupportSection";
 import TrustBadge from "@/components/TrustBadge";
+import VideoBgSection from "@/components/VideoBgSection";
 
 export default function Home() {
   return (
     <>
       <SpecialitiesSection />
       <TrustBadge />
+      <VideoBgSection />
+      <SupportSection />
     </>
   );
 }
