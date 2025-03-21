d4a1e885732855de53c6bd5c44f0f51b122d0881
solemannoushad <solemannoushad31@gmail.com>
2025-03-21 22:33:23 +0500

    responsive added

--- src/components/BlogsSection.tsx
+++ src/components/BlogsSection.tsx
@@ -7,7 +7,7 @@ function BlogsSection() {
   return (
     <section className="section">
       <Heading title="Blogs" />
-      <div className="blogs-section grid grid-cols-1 md:grid-cols-3 gap-4 my-10">
+      <div className="blogs-section grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
         {blogsData.slice(0, 3).map((item, index) => {
             const { title, description, image, url } = item;
             return <BlogCard key={index} title={title} description={description} image={image} url={url} />;
