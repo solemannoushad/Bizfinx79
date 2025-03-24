93c9d960ee9cf7a4b0f6c5ec54fd6a77df4a5007
solemannoushad <solemannoushad31@gmail.com>
2025-03-24 22:12:42 +0500

    font size dicreased for smaller devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-[14px] md:text-[16px]">
       <head>
       <link
           rel="stylesheet"
