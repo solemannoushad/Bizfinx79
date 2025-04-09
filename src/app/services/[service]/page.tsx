8e1407e657ed94da6883976b459e3100d03a8f44
solemannoushad <solemannoushad31@gmail.com>
2025-04-09 16:13:31 +0500

    subService data set

--- src/app/services/[service]/page.tsx
+++ src/app/services/[service]/page.tsx
@@ -1,40 +1,38 @@
-import HomeHero from '@/components/HomeHero'
-import React from 'react'
-import ServiceHero from '../components/ServiceHero'
-import CfoGuide from '../components/CfoGuide'
-import ServiceTemplateHighlight from '../components/ServiceTemplateHiglight'
-import CFOServicesSection from '../components/cfo-services-section'
-import { subServicesdata } from '@/content/ServiceSubPage'
+import React from 'react';
+import { notFound } from 'next/navigation';
+import SubServiceHero from '@/components/SubServiceHero';
+import { subServicesdata } from '@/content/ServiceSubPage';
 
-function page() {
-  
-  return (
-    <div>
-      <ServiceHero heading={subServicesdata[0].heroHeading} subheading={subServicesdata[0].heroSubHeading} />
-      <ServiceTemplateHighlight description={subServicesdata[0].subcardSub} heading={subServicesdata[0].subcardHeading}/>
-      <CfoGuide heading={subServicesdata[0].CardTextHeading}  description={subServicesdata[0].cardTextSub} servicesListData={subServicesdata[0].specialServicesList} />
-      <CFOServicesSection
-        heading="Why Opt for Outsourced CFO Services?"
-        description="The value of CFO advisory services is undeniable. A fractional CFO is pivotal for business longevity, from strategizing growth to organizing finances. Outsourcing these services offers added advantages"
-        bulletPoints={[
-          "Crystal-clear financial insights for audit or fundraising readiness.",
-          "Enhanced efficiency with top-tier accounting or ERP solutions.",
-          "Peace of mind with ensured compliance, immaculate books, and up-to-date financials.",
-        ]}
-        conclusion="Outsourced CFO Services integrate seamlessly into your business model and budget. Entrust your CFO responsibilities to indinero."
-        speechBubbleTitle="Expert management"
-        speechBubbleText="Outsourced CFO services offer growth"
-        phoneImageUrl="/placeholder.svg"
-        phoneImageAlt="Purple telephone illustration"
-        phoneImageWidth={200}
-        phoneImageHeight={300}
-        backgroundColor="bg-yellow-300"
-        accentColor="text-purple-700"
-        textColor="text-purple-950"
-      />
-      
-    </div>
-  )
+interface Props {
+  params: { service: string }
 }
 
-export default page
+function Page({ params }: Props) {
+  const { service } = params;
+
+  const serviceData = subServicesdata.find(item => item.url === service);
+
+  if (!serviceData) return notFound();
+
+  const {
+    title,
+    heroTxt,
+    heroImg,
+    firstCardTitle,
+    firstCardTxt,
+    secondCardTitle,
+    secondCardTxt,
+    coreSpecialities,
+    chooseHeading,
+    choosePoints
+  } = serviceData;
+
+  return (
+    <div>
+      <SubServiceHero txt={heroTxt} image={heroImg} title={title} />
+
+    </div>
+  );
+}
+
+export default Page;
--- src/content/ServiceSubPage.ts
+++ src/content/ServiceSubPage.ts
@@ -1,107 +1,226 @@
-
-export const  subServicesdata=[{
-    url: "taxation-services",
-    heroHeading:
-         "Accounting is the foundation of financial success for any business. <b style='color:#e74c3c;'>BizFinx79</b> offers comprehensive <b style='color:#3498db;'>accounting solutions</b> designed to enhance financial accuracy, compliance, and efficiency. Our expert team ensures seamless <b style='color:#2ecc71;'>financial management</b>, helping businesses optimize cash flow, reduce risks, and make data-driven decisions to maximize profitability.",
-         heroSubHeading:"",
-         heroHeadingButtonText:"Get Started",
-         CardTextHeading:"Stay Ahead with Proactive Financial Management",
-         cardTextSub:  "Achieve financial clarity and control with expert <b style='color:#9b59b6;'>accounting services</b> that empower businesses to maintain <b style='color:#f39c12;'>transparency</b>, <b style='color:#1abc9c;'>optimize resources</b>, and <b style='color:#e67e22;'>drive growth</b> with precision.",
-         subcardHeading:"How BizFinx79 Simplifies Accounting for You",
-         subcardSub:"Accounting can be complex and time-consuming—<b style='color:#e74c3c;'>BizFinx79</b> simplifies the process by combining <b style='color:#2ecc71;'>compliance expertise</b>, <b style='color:#3498db;'>strategic insights</b>, and <b style='color:#9b59b6;'>proven software solutions</b> to minimize errors and improve financial efficiency.",
-         specialServicesList:[
-            {
-                icon: "bookkeeping.svg",
-                title: " Bookkeeping & Financial Record Management",
-                points: [
-                    "Maintain <strong>error-free financial records</strong> with precise bookkeeping.",
-                    "Ensure <strong>seamless transaction tracking</strong> and compliance.",
-                    "Generate <strong>accurate financial reports</strong> for informed decision-making."
-                ]
-            },
-            {
-                icon: "financial-statement.svg",
-                title: " Financial Statement Preparation & Analysis",
-                points: [
-                    "Develop <strong>detailed income statements, balance sheets, and cash flow reports</strong>.",
-                    "<span class='text-red-500'>Perform financial statement analysis including Year-of-Year analysis, ratio analysis, vertical analysis on historical data</span>.",
-                    "Ensure <strong>regulatory compliance</strong> and <strong>tax readiness</strong>.",
-                    "Identify <strong>financial trends</strong> and opportunities for <strong>cost optimization</strong>."
-                ]
-            },
-            {
-                icon: "accounts.svg",
-                title: " Accounts Payable & Receivable Management",
-                points: [
-                    "Optimize <strong>invoice management and vendor payments</strong> to improve cash flow.",
-                    "Ensure timely <strong>collections</strong>/<span class='text-red-500'>payments</span> while maintaining strong client/vendor relationships.",
-                    "Minimize financial discrepancies with <strong>real-time tracking</strong>."
-                ]
-            },
-            {
-                icon: "inventory.svg",
-                title: " Inventory & Cost Management",
-                points: [
-                    "Implement <strong>effective inventory tracking</strong> to optimize stock levels.",
-                    "Minimize <span class='text-red-500'>cost by controlling ordering and carrying cost</span>.",
-                    "Prevent overstocking or shortages with <strong>accurate forecasting</strong>."
-                ]
-            },
-            {
-                icon: "payroll.svg",
-                title: " Payroll Processing & Compliance",
-                points: [
-                    "Ensure <strong>timely, accurate payroll processing</strong> with tax compliance.",
-                    "Automate <strong>salary calculations, deductions, and benefits management</strong>.",
-                    "Stay compliant with local <strong>labor laws and corporate regulations</strong>."
-                ]
-            },
-            {
-                icon: "billing.svg",
-                title: " Customer Billing & Invoicing Automation",
-                points: [
-                    "<strong>Streamline invoicing processes</strong> for faster, more accurate billing.",
-                    "Enhance customer satisfaction with <strong>timely reminders</strong> and payment tracking.",
-                    "Reduce errors and <strong>accelerate cash inflows</strong>."
-                ]
-            },
-            {
-                icon: "tax.svg",
-                title: " Tax Planning, Preparation & Filing",
-                points: [
-                    "Stay compliant with corporate and personal <strong>tax laws</strong> while maximizing returns.",
-                    "Identify <strong>deductions, credits, and tax-saving strategies</strong>.",
-                    "Ensure seamless <strong>tax preparation and filing</strong> to avoid penalties."
-                ]
-            },
-            {
-                icon: "bank.svg",
-                title: " Bank Reconciliation & Fraud Prevention",
-                points: [
-                    "Detect discrepancies early with detailed <strong>bank statement matching</strong>.",
-                    "Prevent fraud and ensure <strong>financial data integrity</strong>.",
-                    "Maintain up-to-date, <strong>accurate financial records</strong>."
-                ]
-            },
-            {
-                icon: "budget.svg",
-                title: " Budgeting, Forecasting & Strategic Planning",
-                points: [
-                    "Develop <strong>customized financial strategies</strong> for sustainable growth.",
-                    "Optimize <strong>resource allocation and expense management</strong>.",
-                    "Predict future trends with <strong>data-driven forecasting models</strong>."
-                ]
-            },
-            {
-                icon: "audit.svg",
-                title: " Audit Support & Compliance Advisory",
-                points: [
-                    "Prepare <strong>audit-ready financial statements</strong> with in-depth documentation.",
-                    "Ensure <strong>compliance with IAS and IFRS</strong>.",
-                    "Get expert support during <strong>regulatory audits and financial reviews</strong>."
-                ]
-            }
-        ]
-      
-      
-}]
+export const subServicesdata = [
+//   {
+//     url: "taxation-services",
+//     heroHeading:
+//       "Accounting is the foundation of financial success for any business. <b style='color:#e74c3c;'>BizFinx79</b> offers comprehensive <b style='color:#3498db;'>accounting solutions</b> designed to enhance financial accuracy, compliance, and efficiency. Our expert team ensures seamless <b style='color:#2ecc71;'>financial management</b>, helping businesses optimize cash flow, reduce risks, and make data-driven decisions to maximize profitability.",
+//     heroSubHeading: "",
+//     heroHeadingButtonText: "Get Started",
+//     CardTextHeading: "Stay Ahead with Proactive Financial Management",
+//     cardTextSub:
+//       "Achieve financial clarity and control with expert <b style='color:#9b59b6;'>accounting services</b> that empower businesses to maintain <b style='color:#f39c12;'>transparency</b>, <b style='color:#1abc9c;'>optimize resources</b>, and <b style='color:#e67e22;'>drive growth</b> with precision.",
+//     subcardHeading: "How BizFinx79 Simplifies Accounting for You",
+//     subcardSub:
+//       "Accounting can be complex and time-consuming—<b style='color:#e74c3c;'>BizFinx79</b> simplifies the process by combining <b style='color:#2ecc71;'>compliance expertise</b>, <b style='color:#3498db;'>strategic insights</b>, and <b style='color:#9b59b6;'>proven software solutions</b> to minimize errors and improve financial efficiency.",
+//     specialServicesList: [
+//       {
+//         icon: "bookkeeping.svg",
+//         title: " Bookkeeping & Financial Record Management",
+//         points: [
+//           "Maintain <strong>error-free financial records</strong> with precise bookkeeping.",
+//           "Ensure <strong>seamless transaction tracking</strong> and compliance.",
+//           "Generate <strong>accurate financial reports</strong> for informed decision-making.",
+//         ],
+//       },
+//       {
+//         icon: "financial-statement.svg",
+//         title: " Financial Statement Preparation & Analysis",
+//         points: [
+//           "Develop <strong>detailed income statements, balance sheets, and cash flow reports</strong>.",
+//           "<span class='text-red-500'>Perform financial statement analysis including Year-of-Year analysis, ratio analysis, vertical analysis on historical data</span>.",
+//           "Ensure <strong>regulatory compliance</strong> and <strong>tax readiness</strong>.",
+//           "Identify <strong>financial trends</strong> and opportunities for <strong>cost optimization</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "accounts.svg",
+//         title: " Accounts Payable & Receivable Management",
+//         points: [
+//           "Optimize <strong>invoice management and vendor payments</strong> to improve cash flow.",
+//           "Ensure timely <strong>collections</strong>/<span class='text-red-500'>payments</span> while maintaining strong client/vendor relationships.",
+//           "Minimize financial discrepancies with <strong>real-time tracking</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "inventory.svg",
+//         title: " Inventory & Cost Management",
+//         points: [
+//           "Implement <strong>effective inventory tracking</strong> to optimize stock levels.",
+//           "Minimize <span class='text-red-500'>cost by controlling ordering and carrying cost</span>.",
+//           "Prevent overstocking or shortages with <strong>accurate forecasting</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "payroll.svg",
+//         title: " Payroll Processing & Compliance",
+//         points: [
+//           "Ensure <strong>timely, accurate payroll processing</strong> with tax compliance.",
+//           "Automate <strong>salary calculations, deductions, and benefits management</strong>.",
+//           "Stay compliant with local <strong>labor laws and corporate regulations</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "billing.svg",
+//         title: " Customer Billing & Invoicing Automation",
+//         points: [
+//           "<strong>Streamline invoicing processes</strong> for faster, more accurate billing.",
+//           "Enhance customer satisfaction with <strong>timely reminders</strong> and payment tracking.",
+//           "Reduce errors and <strong>accelerate cash inflows</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "tax.svg",
+//         title: " Tax Planning, Preparation & Filing",
+//         points: [
+//           "Stay compliant with corporate and personal <strong>tax laws</strong> while maximizing returns.",
+//           "Identify <strong>deductions, credits, and tax-saving strategies</strong>.",
+//           "Ensure seamless <strong>tax preparation and filing</strong> to avoid penalties.",
+//         ],
+//       },
+//       {
+//         icon: "bank.svg",
+//         title: " Bank Reconciliation & Fraud Prevention",
+//         points: [
+//           "Detect discrepancies early with detailed <strong>bank statement matching</strong>.",
+//           "Prevent fraud and ensure <strong>financial data integrity</strong>.",
+//           "Maintain up-to-date, <strong>accurate financial records</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "budget.svg",
+//         title: " Budgeting, Forecasting & Strategic Planning",
+//         points: [
+//           "Develop <strong>customized financial strategies</strong> for sustainable growth.",
+//           "Optimize <strong>resource allocation and expense management</strong>.",
+//           "Predict future trends with <strong>data-driven forecasting models</strong>.",
+//         ],
+//       },
+//       {
+//         icon: "audit.svg",
+//         title: " Audit Support & Compliance Advisory",
+//         points: [
+//           "Prepare <strong>audit-ready financial statements</strong> with in-depth documentation.",
+//           "Ensure <strong>compliance with IAS and IFRS</strong>.",
+//           "Get expert support during <strong>regulatory audits and financial reviews</strong>.",
+//         ],
+//       },
+//     ],
+//   },
+  {
+    url: "accounting-services",
+    title: "Accounting Services",
+    heroTxt:
+      "Accounting is the foundation of financial success for any business. BizFinx79 offers comprehensive accounting solutions designed to enhance financial accuracy, compliance, and efficiency. Our expert team ensures seamless financial management, helping businesses optimize cash flow, reduce risks, and make data-driven decisions to maximize profitability.",
+    heroImg: "Money income-rafiki.png",
+    firstCardTitle: "Stay Ahead with Proactive Financial Management",
+    firstCardTxt:
+      "Achieve financial clarity and control with expert accounting services that empower businesses to maintain transparency, optimize resources, and drive growth with precision.",
+    secondCardTitle: "How BizFinx79 Simplifies Accounting for You",
+    secondCardTxt:
+      "Accounting can be complex and time-consuming—BizFinx79 simplifies the process by combining compliance expertise, strategic insights, and proven software solutions to minimize errors and improve financial efficiency.",
+    coreSpecialities: {
+      heading: "Our Comprehensive Accounting Services",
+      specialities: [
+        {
+          title: "Bookkeeping & Financial Record Management",
+          points: [
+            "Maintain error-free financial records with precise bookkeeping.",
+            "Ensure seamless transaction tracking and compliance.",
+            "Generate accurate financial reports for informed decision-making.",
+          ],
+        },
+        {
+          title: "Financial Statement Preparation & Analysis",
+          points: [
+            "Develop detailed income statements, balance sheets, and cash flow reports.",
+            "Perform financial statement analysis including Year-of-Year analysis, ratio analysis, vertical analysis on historical data.",
+            "Ensure regulatory compliance and tax readiness.",
+            "Identify financial trends and opportunities for cost optimization.",
+          ],
+        },
+        {
+          title: "Accounts Payable & Receivable Management",
+          points: [
+            "Optimize invoice management and vendor payments to improve cash flow.",
+            "Ensure timely collections/payments while maintaining strong client/vendor relationships.",
+            "Minimize financial discrepancies with real-time tracking.",
+          ],
+        },
+        {
+          title: "Inventory & Cost Management",
+          points: [
+            "Implement effective inventory tracking to optimize stock levels.",
+            "Minimize cost by controlling ordering and carrying cost.",
+            "Prevent overstocking or shortages with accurate forecasting.",
+          ],
+        },
+        {
+          title: "Payroll Processing & Compliance",
+          points: [
+            "Ensure timely, accurate payroll processing with tax compliance.",
+            "Automate salary calculations, deductions, and benefits management.",
+            "Stay compliant with local labor laws and corporate regulations.",
+          ],
+        },
+        {
+          title: "Customer Billing & Invoicing Automation",
+          points: [
+            "Streamline invoicing processes for faster, more accurate billing.",
+            "Enhance customer satisfaction with timely reminders and payment tracking.",
+            "Reduce errors and accelerate cash inflows.",
+          ],
+        },
+        {
+          title: "Tax Planning, Preparation & Filing",
+          points: [
+            "Stay compliant with corporate and personal tax laws while maximizing returns.",
+            "Identify deductions, credits, and tax-saving strategies.",
+            "Ensure seamless tax preparation and filing to avoid penalties.",
+          ],
+        },
+        {
+          title: "Bank Reconciliation & Fraud Prevention",
+          points: [
+            "Detect discrepancies early with detailed bank statement matching.",
+            "Prevent fraud and ensure financial data integrity.",
+            "Maintain up-to-date, accurate financial records.",
+          ],
+        },
+        {
+          title: "Budgeting, Forecasting & Strategic Planning",
+          points: [
+            "Develop customized financial strategies for sustainable growth.",
+            "Optimize resource allocation and expense management.",
+            "Predict future trends with data-driven forecasting models.",
+          ],
+        },
+        {
+          title: "Audit Support & Compliance Advisory",
+          points: [
+            "Prepare audit-ready financial statements with in-depth documentation.",
+            "Ensure compliance with IAS and IFRS.",
+            "Get expert support during regulatory audits and financial reviews.",
+          ],
+        },
+      ],
+    },
+    chooseHeading: "Why Choose BizFinx79?",
+    choosePoints: [
+        {
+            title: "Expert-Led Financial Management",
+            txt: "Combining deep expertise with reliable software and proven methodologies."
+        },
+        {
+            title: "Industry-Specific Solutions",
+            txt: "Tailored financial strategies for various business sectors."
+        },
+        {
+            title: "Regulatory Compliance & Risk Management",
+            txt: "Adhering to global financial and tax regulations."
+        },
+        {
+            title: "Transparent Reporting & Insights",
+            txt: "Providing clear, actionable data to guide strategic decisions."
+        },
+    ]
+  },
+];
