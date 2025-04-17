3167f40448c000c217096889ced40f9c6d487041
solemannoushad <solemannoushad31@gmail.com>
2025-04-17 23:50:39 +0500

    blogs title updated

--- src/content/blogs.ts
+++ src/content/blogs.ts
@@ -2,31 +2,31 @@ export const blogsData = [
     {
         url: '',
         image: 'https://www.auxis.com/wp-content/uploads/2024/03/image-51-1-1-1024x683.png',
-        title: 'RCM Automation in Healthcare for a Better Patient Experience',
+        title: 'Advance in your finance and accounting careers with top technical skills',
         description: 'Auxis RCM automation transforms modical supplier\'s insurance verification and prior authorization'
     },
     {
         url: '',
         image: 'https://www.auxis.com/wp-content/uploads/2024/03/image-51-1-1-1024x683.png',
-        title: 'RCM Automation in Healthcare for a Better Patient Experience',
+        title: 'The Biggest Technology Trends In Accounting And Finance',
         description: 'Auxis RCM automation transforms modical supplier\'s insurance verification and prior authorization'
     },
     {
         url: '',
         image: 'https://www.auxis.com/wp-content/uploads/2024/03/image-51-1-1-1024x683.png',
-        title: 'RCM Automation in Healthcare for a Better Patient Experience',
+        title: "Finance vs. Accounting: What's the Difference and Why It Matters",
         description: 'Auxis RCM automation transforms modical supplier\'s insurance verification and prior authorization'
     },
     {
         url: '',
         image: 'https://www.auxis.com/wp-content/uploads/2024/03/image-51-1-1-1024x683.png',
-        title: 'RCM Automation in Healthcare for a Better Patient Experience',
+        title: 'How AI, Community, and Sustainability are Reshaping Accounting and Finance in 2025',
         description: 'Auxis RCM automation transforms modical supplier\'s insurance verification and prior authorization'
     },
     {
         url: '',
         image: 'https://www.auxis.com/wp-content/uploads/2024/03/image-51-1-1-1024x683.png',
-        title: 'RCM Automation in Healthcare for a Better Patient Experience',
+        title: 'FinTech Revolution: Understanding the Disruption and Avoiding the Hype',
         description: 'Auxis RCM automation transforms modical supplier\'s insurance verification and prior authorization'
     }
 ]
