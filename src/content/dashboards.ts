12b4d6ebeb3345698f7b0e3e72638741c384ca31
solemannoushad <solemannoushad31@gmail.com>
2025-04-20 23:44:21 +0500

    dashboard text updated

--- src/content/dashboards.ts
+++ src/content/dashboards.ts
@@ -1,30 +1,33 @@
 export const dashboardData = [
     {
-        "heading": "BizFinx79 Dashboard",
-        "subheading": "BizFinx79 Dashboard is a powerful tool that provides you with real-time insights into your financial performance. With its user-friendly interface and advanced analytics, you can easily track your income, expenses, and investments, helping you make informed decisions for your financial future.",
-        "images": [
-            "dashboard-1-1.png",
-            "dashboard-1-2.png",
-            "dashboard-1-3.png",
-            "dashboard-1-4.png",
-            "dashboard-1-5.png",
-            "dashboard-1-6.png",
-            "dashboard-1-7.png",
-            "dashboard-1-8.png",
-        ]
+      heading: "Performance Analytics Dashboard",
+      subheading:
+        "A deep dive into platform performance, user activity, and key growth metrics. These charts reflect how strategic feature implementation and backend optimization translated into measurable results across engagement, speed, and efficiency.",
+      images: [
+        "dashboard-1-1.png",
+        "dashboard-1-2.png",
+        "dashboard-1-3.png",
+        "dashboard-1-4.png",
+        "dashboard-1-5.png",
+        "dashboard-1-6.png",
+        "dashboard-1-7.png",
+        "dashboard-1-8.png",
+      ],
     },
     {
-        "heading": "BizFinx79 Dashboard2",
-        "subheading": "BizFinx79 Dashboard is a powerful tool that provides you with real-time insights into your financial performance. With its user-friendly interface and advanced analytics, you can easily track your income, expenses, and investments, helping you make informed decisions for your financial future.",
-        "images": [
-            "dashboard-2-1.png",
-            "dashboard-2-2.png",
-            "dashboard-2-3.png",
-            "dashboard-2-4.png",
-            "dashboard-2-5.png",
-            "dashboard-2-6.png",
-            "dashboard-2-7.png",
-            "dashboard-2-8.png",
-        ]
-    }
-]
+      heading: "Operational Efficiency Dashboard",
+      subheading:
+        "Focused on system performance, automation, and workflow enhancements, this dashboard captures the success of smart engineering decisions. The data visualizations highlight reduced bottlenecks, improved task handling, and an overall boost in operational flow.",
+      images: [
+        "dashboard-2-1.png",
+        "dashboard-2-2.png",
+        "dashboard-2-3.png",
+        "dashboard-2-4.png",
+        "dashboard-2-5.png",
+        "dashboard-2-6.png",
+        "dashboard-2-7.png",
+        "dashboard-2-8.png",
+      ],
+    },
+  ]
+  
