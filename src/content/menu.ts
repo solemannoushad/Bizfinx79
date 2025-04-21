c1d37a4911b5c2006a774642b4bfdaceb7bc448d
solemannoushad <solemannoushad31@gmail.com>
2025-04-21 20:43:36 +0500

    2nd dropdown content updated

--- src/content/menu.ts
+++ src/content/menu.ts
@@ -14,112 +14,321 @@ export const menu = [
                 path: '/services/accounting-services',
                 dropdown: [
                     {
-                        title: 'Bookkeeping & Financial Record Management',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Bookkeeping & Financial Record Management",
+                      path: "/services/accounting-services#bookkeeping-financial-record-management",
+                      dropdown: []
                     },
                     {
-                        title: 'Bookkeeping & Financial Record Management',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Financial Statement Preparation & Analysis",
+                      path: "/services/accounting-services#financial-statement-preparation-analysis",
+                      dropdown: []
                     },
                     {
-                        title: 'Bookkeeping & Financial Record Management',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Accounts Payable & Receivable Management",
+                      path: "/services/accounting-services#accounts-payable-receivable-management",
+                      dropdown: []
                     },
                     {
-                        title: 'Bookkeeping & Financial Record Management',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Inventory & Cost Management",
+                      path: "/services/accounting-services#inventory-cost-management",
+                      dropdown: []
                     },
                     {
-                        title: 'Bookkeeping & Financial Record Management',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Payroll Processing & Compliance",
+                      path: "/services/accounting-services#payroll-processing-compliance",
+                      dropdown: []
                     },
                     {
-                        title: 'Bookkeeping & Financial Record Management',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Customer Billing & Invoicing Automation",
+                      path: "/services/accounting-services#customer-billing-invoicing-automation",
+                      dropdown: []
                     },
-                ]
+                    {
+                      title: "Tax Planning, Preparation & Filing",
+                      path: "/services/accounting-services#tax-planning-preparation-filing",
+                      dropdown: []
+                    },
+                    {
+                      title: "Bank Reconciliation & Fraud Prevention",
+                      path: "/services/accounting-services#bank-reconciliation-fraud-prevention",
+                      dropdown: []
+                    },
+                    {
+                      title: "Budgeting, Forecasting & Strategic Planning",
+                      path: "/services/accounting-services#budgeting-forecasting-strategic-planning",
+                      dropdown: []
+                    },
+                    {
+                      title: "Audit Support & Compliance Advisory",
+                      path: "/services/accounting-services#audit-support-compliance-advisory",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Taxation Services',
                 path: '/services/taxation-services',
                 dropdown: [
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Corporate Tax Returns Preparation & Filing",
+                      path: "/services/taxation-services#corporate-tax-returns-preparation-filing",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Sales Tax / Value Added Tax (VAT) Services",
+                      path: "/services/taxation-services#sales-tax-value-added-tax-vat-services",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Withholding Tax Compliance",
+                      path: "/services/taxation-services#withholding-tax-compliance",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Tax Advisory",
+                      path: "/services/taxation-services#tax-advisory",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "ZATCA Compliance",
+                      path: "/services/taxation-services#zatca-compliance",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "Personal Tax Services",
+                      path: "/services/taxation-services#personal-tax-services",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
+                      title: "International Taxation",
+                      path: "/services/taxation-services#international-taxation",
+                      dropdown: []
                     },
                     {
-                        title: 'Corporate Tax Returns Preparation & Filing',
-                        path: '/services/accounting-services#bookkeeping-financial-record-management',
-                        dropdown: []
-                    },
-                ]
+                      title: "Tax Audit Representation",
+                      path: "/services/taxation-services#tax-audit-representation",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Financial Consulting',
                 path: '/services/financial-consulting',
-                dropdown: []
+                dropdown: [
+                    {
+                      title: "Business Valuation",
+                      path: "/services/financial-consulting#business-valuation",
+                      dropdown: []
+                    },
+                    {
+                      title: "Financial Planning",
+                      path: "/services/financial-consulting#financial-planning",
+                      dropdown: []
+                    },
+                    {
+                      title: "Cash Flow Management",
+                      path: "/services/financial-consulting#cash-flow-management",
+                      dropdown: []
+                    },
+                    {
+                      title: "Investment Advisory",
+                      path: "/services/financial-consulting#investment-advisory",
+                      dropdown: []
+                    },
+                    {
+                      title: "Risk Management",
+                      path: "/services/financial-consulting#risk-management",
+                      dropdown: []
+                    },
+                    {
+                      title: "Debt Management",
+                      path: "/services/financial-consulting#debt-management",
+                      dropdown: []
+                    },
+                    {
+                      title: "Compliance & Regulatory Services",
+                      path: "/services/financial-consulting#compliance-regulatory-services",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Business Advisory Services',
                 path: '/services/business-advisory-services',
-                dropdown: []
+                dropdown: [
+                    {
+                      title: "Startup Consulting",
+                      path: "/services/business-advisory-services#startup-consulting",
+                      dropdown: []
+                    },
+                    {
+                      title: "Business Plan Development",
+                      path: "/services/business-advisory-services#business-plan-development",
+                      dropdown: []
+                    },
+                    {
+                      title: "Projections",
+                      path: "/services/business-advisory-services#projections",
+                      dropdown: []
+                    },
+                    {
+                      title: "Pitch Deck",
+                      path: "/services/business-advisory-services#pitch-deck",
+                      dropdown: []
+                    },
+                    {
+                      title: "Mergers & Acquisitions (M&A)",
+                      path: "/services/business-advisory-services#mergers-acquisitions-ma",
+                      dropdown: []
+                    },
+                    {
+                      title: "Exit Strategy Planning",
+                      path: "/services/business-advisory-services#exit-strategy-planning",
+                      dropdown: []
+                    },
+                    {
+                      title: "Cost Reduction Analysis",
+                      path: "/services/business-advisory-services#cost-reduction-analysis",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Compliance and Regulatory Services',
                 path: '/services/compliance-and-regulatory-services',
-                dropdown: []
+                dropdown: [
+                    {
+                      title: "Corporate Compliance",
+                      path: "/services/compliance-and-regulatory-services#corporate-compliance",
+                      dropdown: []
+                    },
+                    {
+                      title: "GST/VAT Compliance",
+                      path: "/services/compliance-and-regulatory-services#gst-vat-compliance",
+                      dropdown: []
+                    },
+                    {
+                      title: "SEC Filings",
+                      path: "/services/compliance-and-regulatory-services#sec-filings",
+                      dropdown: []
+                    },
+                    {
+                      title: "Annual Report Preparation",
+                      path: "/services/compliance-and-regulatory-services#annual-report-preparation",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Specialized Services',
                 path: '/services/specialized-services',
-                dropdown: []
+                dropdown: [
+                    {
+                      title: "Nonprofit Accounting",
+                      path: "/services/specialized-services#nonprofit-accounting",
+                      dropdown: []
+                    },
+                    {
+                      title: "Real Estate Accounting",
+                      path: "/services/specialized-services#real-estate-accounting",
+                      dropdown: []
+                    },
+                    {
+                      title: "E-commerce Accounting",
+                      path: "/services/specialized-services#e-commerce-accounting",
+                      dropdown: []
+                    },
+                    {
+                      title: "Forensic Accounting",
+                      path: "/services/specialized-services#forensic-accounting",
+                      dropdown: []
+                    },
+                    {
+                      title: "International Accounting",
+                      path: "/services/specialized-services#international-accounting",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Additional Services',
                 path: '/services/additional-services',
-                dropdown: []
+                dropdown: [
+                    {
+                      title: "Virtual CFO Services",
+                      path: "/services/additional-services#virtual-cfo-services",
+                      dropdown: []
+                    },
+                    {
+                      title: "Estate Planning",
+                      path: "/services/additional-services#estate-planning",
+                      dropdown: []
+                    },
+                    {
+                      title: "Retirement Planning",
+                      path: "/services/additional-services#retirement-planning",
+                      dropdown: []
+                    },
+                    {
+                      title: "Insurance Advisory",
+                      path: "/services/additional-services#insurance-advisory",
+                      dropdown: []
+                    }
+                  ]
+                  
             },
             {
                 title: 'Cost Management',
                 path: '/services/cost-management',
-                dropdown: []
+                dropdown: [
+                    {
+                      title: "Standard Costing",
+                      path: "/services/cost-management#standard-costing",
+                      dropdown: []
+                    },
+                    {
+                      title: "Conducting Variance Analysis",
+                      path: "/services/cost-management#conducting-variance-analysis",
+                      dropdown: []
+                    },
+                    {
+                      title: "Budgeting & Forecasting",
+                      path: "/services/cost-management#budgeting-forecasting",
+                      dropdown: []
+                    },
+                    {
+                      title: "Cost of Production Report",
+                      path: "/services/cost-management#cost-of-production-report",
+                      dropdown: []
+                    },
+                    {
+                      title: "Handling Bill of Material & Reporting",
+                      path: "/services/cost-management#handling-bill-of-material-reporting",
+                      dropdown: []
+                    },
+                    {
+                      title: "Cost Controlling Measures",
+                      path: "/services/cost-management#cost-controlling-measures",
+                      dropdown: []
+                    },
+                    {
+                      title: "Implementing Lean Operations",
+                      path: "/services/cost-management#implementing-lean-operations",
+                      dropdown: []
+                    },
+                    {
+                      title: "Preparing Cost Reports",
+                      path: "/services/cost-management#preparing-cost-reports",
+                      dropdown: []
+                    }
+                  ]
+                  
             }
         ]
     },
