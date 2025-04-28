444cff604488b265ec65af43de43158cc1f9cd1f
solemannoushad <solemannoushad31@gmail.com>
2025-04-28 23:26:10 +0500

    contact updated in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,6 +16,10 @@ function page() {
             name: 'phone',
             text: '00966-538320746'
         },
+        {
+            name: 'phone',
+            text: '0092-3214503133'
+        },
     ]
 
   return (
@@ -29,7 +33,7 @@ function page() {
             <div className='flex my-8 justify-center lg:flex-row flex-col'>
                 {icons.map((i) => {
                     return(
-                        <div key={i.name} className='flex items-center mx-2 cursor-pointer group'>                        
+                        <div key={i.text} className='flex items-center mx-2 cursor-pointer group'>                        
                             <div className='border-2 border-foreground bg-foreground inline-flex items-center justify-normal rounded-full cursor-pointer group-hover:bg-white transition-all duration-100'>
                                 <i className={`iconoir-${i.name} text-white p-2 text-xl transition-all duration-500 group-hover:text-foreground`}></i>
                             </div>
