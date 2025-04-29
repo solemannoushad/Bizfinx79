ee9e042d2c15f9707ae20f60b681a3a8ba78ae7c
solemannoushad <solemannoushad31@gmail.com>
2025-04-29 15:03:13 +0500

    teams section made responsive

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -11,11 +11,11 @@ function Team() {
             teamData.map((data, index) => {
                 const {img, name, title, description} = data;
                 return(
-                    <div className={`team-card flex ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} gap-16 items-center justify-center`} key={name}>
-                        <div className='team-img w-[50%] h-[550px] rounded-lg shadow-lg shadow-black overflow-hidden'>
+                    <div className={`team-card flex flex-col ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'} gap-16 items-center justify-center`} key={name}>
+                        <div className='team-img w-full md:w-1/2 h-[550px] rounded-lg shadow-lg shadow-black overflow-hidden'>
                             <img className="w-full h-full object-cover" src={`/images/${img}`} alt={name} />
                         </div>
-                        <div className="w-1/2 flex flex-col gap-3">
+                        <div className="w-full md:w-1/2 flex flex-col gap-3">
                             <h3 className='text-4xl font-medium'>{name}</h3>
                             <h4 className='text-3xl font-normal'>{title}</h4>
                             <p className="text-justify">{description}</p>
